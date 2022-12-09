7a3091c4cbaa38659606a6985a10e5f3f7ad2c01
salahcst344 <salahcst344@gmail.com>
2022-12-09 18:04:34 +0200

    Create database class and it's const fields

---
+++ src/main/java/com/mycompany/hyper_market/DB.java
@@ -0,0 +1,11 @@
+package com.mycompany.hyper_market;
+
+import java.sql.*;
+
+final public class DB {
+    final static private String CLASS_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    final static private String CONNICTION_URL = "jdbc:sqlserver://localhost:1433;databaseName=HyperMarket;encrypt=true;trustServerCertificate=true;";
+    final static private String USER_NAME = "sa";
+    final static private String PASSWORD = "1234";
+    
+}
