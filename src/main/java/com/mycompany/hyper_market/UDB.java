5a7c6020771494cbc0c85439110e31e1bb3234bf
salahcst344 <salahcst344@gmail.com>
2022-12-13 14:35:34 +0200

    Create UDB isUserIdUnique method

--- src/main/java/com/mycompany/hyper_market/UDB.java
+++ src/main/java/com/mycompany/hyper_market/UDB.java
@@ -5,27 +5,16 @@ import java.util.ArrayList;
 
 public class UDB extends DB {
 
-    static public boolean isUserNameUnique(String userName) throws SQLException, ClassNotFoundException {
+    public static boolean isUserNameUnique(String userName) throws SQLException, ClassNotFoundException {
         String query = "SELECT userName FROM users WHERE userName = '" + userName + "'";
 
         return isUnique(query);
     }
 
-    static public boolean isUserIdUnique(int userId) throws SQLException, ClassNotFoundException {
+    public static boolean isUserIdUnique(int userId) throws SQLException, ClassNotFoundException {
         String query = "SELECT id FROM users WHERE id = '" + userId + "'";
 
         return isUnique(query);
-    } 
-    
-    static public String login(String userName, String password) throws SQLException, ClassNotFoundException {
-        String query = "SELECT * FROM users WHERE userName = '" + userName + "' and password = '" + password + "'";
-        
-        ResultSet res = DQLQuery(query);
-        
-        if(!res.next()) return null;
-        
-        return "" + res.getInt("id") + " " + res.getNString("name") + " " + res.getNString("userName")
-                + " " + res.getNString("password") + " " + res.getNString("type");
-        
     }
+    
 }
