cf263ab81a442b45e6ba8a5494861fe058d7ddc8
salahcst344 <salahcst344@gmail.com>
2022-12-13 22:25:30 +0200

    Create main class

---
+++ src/hypermarket/HyperMarket.java
@@ -0,0 +1,23 @@
+package hypermarket;
+
+import GUI.Admin;
+import GUI.login;
+
+/**
+ *
+ * @author Ali
+ */
+public class HyperMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login f1 = new login();
+        Admin a = new Admin();
+        a.setVisible(true);
+//        f1.setVisible(true);
+          
+    }
+    
+}
