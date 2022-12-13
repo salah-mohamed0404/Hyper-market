28c99dda929599df1cabdd2fb13fe8bc57c54cf7
salahcst344 <salahcst344@gmail.com>
2022-12-13 17:41:46 +0200

    Create ADB add method

--- src/main/java/Database/ADB.java
+++ src/main/java/Database/ADB.java
@@ -1,5 +1,16 @@
 package Database;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ADB extends DB {
-    
+    public static int add(Action action, int userId) throws SQLException, ClassNotFoundException {
+        String query = "INSERT INTO users VALUES("
+                + action.id + ", '" + action.action + "', " + action.createdAt + ", " + userId + ")";
+
+        DMLQuery(query);
+        
+        return getId("user_actions", "id = " + userId);
+    }
+
 }
