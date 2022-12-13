19192d0818d8dceefeb8a476e306c046cb0c9d30
salahcst344 <salahcst344@gmail.com>
2022-12-13 18:53:28 +0200

    Create PDB add method

--- src/main/java/Database/PDB.java
+++ src/main/java/Database/PDB.java
@@ -1,5 +1,23 @@
 package Database;
 
-public class PDB {
-    
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PDB extends DB{
+
+    public static int add(Product product) throws SQLException, ClassNotFoundException {
+        String query = "INSERT INTO products VALUES("
+                + "'" + product.name + "', "
+                + product.price + ", "
+                + product.offerPrice + ", "
+                + "'" + product.expireDate + "', "
+                + "'" + product.type + "', "
+                + "'" + product.addedAt + "', "
+                + ")";
+
+        DMLQuery(query);
+
+        return getLastRecordIdAdded("products");
+    }
+
 }
