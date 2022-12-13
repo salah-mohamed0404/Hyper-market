a5082ff48238f7c9434f5a523ee976752e2d2eb5
salahcst344 <salahcst344@gmail.com>
2022-12-13 14:33:49 +0200

    Refactor to be super and abstract class

--- src/main/java/com/mycompany/hyper_market/DB.java
+++ src/main/java/com/mycompany/hyper_market/DB.java
@@ -2,65 +2,34 @@ package com.mycompany.hyper_market;
 
 import java.sql.*;
 
-final public class DB {
-    final static private String CLASS_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    final static private String CONNICTION_URL = "jdbc:sqlserver://localhost:1433;databaseName=HyperMarket;encrypt=true;trustServerCertificate=true;";
-    final static private String USER_NAME = "sa";
-    final static private String PASSWORD = "1234";
+public abstract class DB {
 
-    static private Connection connect() throws SQLException {
-        try {
-            Class.forName(CLASS_NAME);
-            return DriverManager.getConnection(CONNICTION_URL, USER_NAME, PASSWORD);
-        } catch (Exception e) {
-            return null;
-        }
+    final private static String CLASS_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    final private static String CONNICTION_URL = "jdbc:sqlserver://localhost:1433;databaseName=HyperMarket;encrypt=true;trustServerCertificate=true;";
+    final private static String USER_NAME = "sa";
+    final private static String PASSWORD = "1234";
+
+    protected static Connection connect() throws SQLException, ClassNotFoundException {
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(CONNICTION_URL, USER_NAME, PASSWORD);
     }
 
-    static private boolean DMLQuery(String query) {
-        try {
-            Connection conn = connect();
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate(query);
-
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
+    protected static void DMLQuery(String query) throws SQLException, ClassNotFoundException {
+        Connection conn = connect();
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(query);
     }
 
-    static private ResultSet DQLQuery(String query) {
-        try {
-            Connection conn = connect();
-            PreparedStatement stmt = conn.prepareStatement(query);
+    protected static ResultSet DQLQuery(String query) throws SQLException, ClassNotFoundException {
+        Connection conn = connect();
+        PreparedStatement stmt = conn.prepareStatement(query);
 
-            return stmt.executeQuery();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+        return stmt.executeQuery();
     }
 
-    static private boolean isUnique(String query) {
-        try {
-                ResultSet r = DQLQuery(query);
-                return !r.next();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                return false;
-            }
-    }
+    protected static boolean isUnique(String query) throws SQLException, ClassNotFoundException {
+        ResultSet r = DQLQuery(query);
 
-    static public boolean isUserNameUnique(String userName) {
-        String query = "SELECT userName FROM users WHERE userName = '" + userName + "'";
-
-        return isUnique(query);
-    }
-
-    static public boolean isUserIdUnique(int userId) {
-        String query = "SELECT id FROM users WHERE id = '" + userId + "'";
-
-        return isUnique(query);
+        return !r.next();
     }
 }
