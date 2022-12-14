c5b5e2057de905e846ba10b25e6a45cf8ef925a3
salahcst344 <salahcst344@gmail.com>
2022-12-14 23:39:38 +0200

    Make class abstract

--- src/User/User.java
+++ src/User/User.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package User;
 
 import Database.ADB;
@@ -11,9 +7,9 @@ import java.util.ArrayList;
 
 /**
  *
- * @author DELL
+ * @author Abd El-halim
  */
-public class User {
+public abstract class User {
 
     /* data field */
     private int id;
